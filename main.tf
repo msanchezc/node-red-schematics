fc6f3565126584d7ffbab86eac326e1bdf1b8f1f
Manuel Sanchez <manuel.sanchez@linux.com>
2022-05-13 09:30:58 -0400

    Initial commit

---
+++ main.tf
@@ -0,0 +1,53 @@
+resource "null_resource" "prepare_app_zip" {
+  provisioner "local-exec" {
+    command = <<EOF
+        mkdir -p /tmp/app
+        cd /tmp/app
+        git init
+        git remote add origin https://github.com/msanchezc/node-red-app
+        git fetch
+        git checkout -t origin/master
+        zip -r app.zip *
+        
+EOF
+
+  }
+}
+
+data "ibm_space" "space" {
+  org = var.org
+  space = var.space
+}
+
+resource "ibm_service_instance" "service-instance" {
+  name       = "cloudant"
+  space_guid = data.ibm_space.space.id
+  service    = "cloudantNoSQLDB"
+  plan       = "lite"
+  tags       = ["cluster-service", "cluster-bind"]
+}
+
+resource "ibm_service_key" "service-key" {
+  name                  = "cloudant-key"
+  service_instance_guid = ibm_service_instance.service-instance.id
+}
+
+resource "ibm_app" "app" {
+  depends_on = [
+    ibm_service_key.service-key,
+    null_resource.prepare_app_zip,
+  ]
+  name              = var.app_name
+  space_guid        = data.ibm_space.space.id
+  app_path          = "/tmp/app.zip"
+  wait_time_minutes = 10
+
+  buildpack  = "https://github.com/cloudfoundry/nodejs-buildpack.git"
+  
+  memory                = 256
+  instances             = 1
+  disk_quota            = 512
+  service_instance_guid = [ibm_service_instance.service-instance.id]
+  app_version           = "1"
+  command               = "npm start"
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "space" {
+  default = "space"
+}
+
+variable "org" {
+  default = "org"
+}
+
+variable "app_name" {
+  default = "nodered"
+}
